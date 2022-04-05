38cfbb0c1e202ee0bdd32aebe4681b84f96fe825
Antonio Ruggiero <antonrugg@gmail.com>
2022-04-05 18:28:00 +0200

    end practice - end bonus

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <JumboTron/>
+    <HelloWorld msg="La mia prima app con Vue CLI!"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/Title.vue'
+import JumboTron from './components/Hero.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    JumboTron
   }
 }
 </script>
---
+++ src/components/Hero.vue
@@ -0,0 +1,30 @@
+<template>
+ <img src="https://images.unsplash.com/photo-1612311683149-939135aa6afa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" alt="bird jumbotron">
+</template>
+
+<script>
+export default {
+  name: 'JumboTron',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
