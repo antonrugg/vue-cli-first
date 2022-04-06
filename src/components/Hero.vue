d4d8e58e87e58568ca2b265bc0385fe216fd5c01
Antonio Ruggiero <antonrugg@gmail.com>
2022-04-06 08:06:32 +0200

    cleaning components from unused styles

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,5 +1,5 @@
 <template>
- <img src="https://images.unsplash.com/photo-1612311683149-939135aa6afa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" alt="bird jumbotron">
+ <img src="../assets/jumbo-bird.jpg" alt="bird jumbotron">
 </template>
 
 <script>
@@ -13,18 +13,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+img{
+  width: 400px;
+  height: 500px;
 }
 </style>
